72520368817bc1d2c199bb589b1456f726902b08
JNavAnt <43210295+JNavAnt@users.noreply.github.com>
2020-09-25 12:55:21 -0600

    Add files via upload
    agregados archivos de prestamo e inventario

---
+++ InventarioDasc/prestamo-cons.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../styles/normalize.css">
+        <link rel="stylesheet" href="../styles/inventario.css">
+        <title>Inventario</title>
+        <?php include ('header.html');?>
+    </head>
+    <body>
+        <div>
+            <button>Agregar</button>
+        </div>
+        <div>
+            <div id="loan-card">
+                <h1 id="loan-name"></h1>
+                <h1>Fecha y hora pedido</h1>
+                <h1 id="loan-date-enter"></h1>
+                <h1>Fecha y hora devolucion</h1>
+                <h1 id="loan-date-exit"></h1>
+                <button>Devolver</button>
+            </div>
+        </div>
+    </body>
+</html>
