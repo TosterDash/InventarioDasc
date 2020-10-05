8474b2137e8a53b6be9ffb5ff402defc2158db28
pish18 <paulasoqui69@gmail.com>
2020-10-04 18:16:46 -0600

    Detalles del menú
    Mejoras al menú:
    - cambios en el estilo
    - referenciación correcta de los enlaces del header.

--- InventarioDasc/index.php
+++ InventarioDasc/index.php
@@ -15,20 +15,20 @@ if(!isset($_SESSION['nombre']))
 
 ?>
 
-
 <!DOCTYPE html>
 <html>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<?php include ('header.html');?>
 	<!--SITE STYLE SHEETS-->
 	<link rel="stylesheet" href="../styles/normalize.css">
 	<title>INICIO</title>
 <head>
-	<?php include ('header.html');?>
+		
 </head>
 <body>
+<<<<<<< Updated upstream
 	<div>
 		<button>
 			MAPA
@@ -39,7 +39,33 @@ if(!isset($_SESSION['nombre']))
 		<button>
 			PRESTAMOS
 		</button>
+=======
+	<div class="slider">
+		<ul>
+			<li>
+				<img src="../resources/campus_mapa.jpg" alt="">
+			</li>
+			<li>
+				<img src="../resources/campus_mapa.jpg" alt="">
+			</li>
+			<li>
+				<img src="../resources/campus_mapa.jpg" alt="">
+			</li>
+			<li>
+			<img src="../resources/campus_mapa.jpg" alt="">
+			</li>
+		</ul>
+>>>>>>> Stashed changes
 	</div>
+	<div class="btn-container" >
+			<a href="mapa.php"><img src="../resources/1x/mapa_icon.png"></a> 
+			<a href="inventario-cons.php"><img src="../resources//1x/inven_icon.png"></a>
+			<a href="prestamo-cons.php"><img src="../resources//1x/prest_icon.png"></a>	
+		</div>	
 </body>
+<<<<<<< Updated upstream
 	
 </html>
+=======
+</html>
+>>>>>>> Stashed changes
---
+++ InventarioDasc/prestamo-cons.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <?php include ('header.html');?>
+        <link rel="stylesheet" href="../styles/normalize.css">
+        <link rel="stylesheet" href="../styles/loanStyle.css">
+        
+        <title>Inventario</title>
+    </head>
+    <body>
+        <div class="add-button">
+            <button>Agregar</button>
+        </div>
+        <div>
+            <div id="loan-card">
+                <h1 id="loan-name"></h1>
+                <h1>Fecha y hora pedido</h1>
+                <h1 id="loan-date-enter"></h1>
+                <h1>Fecha y hora devolucion</h1>
+                <h1 id="loan-date-exit"></h1>
+                <button>Devolver</button>
+            </div>
+        </div>
+    </body>
+</html>
