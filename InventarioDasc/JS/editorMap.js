5065bbe30e575bb297dbe36051e885341c53ab03
TosterDash <43210121+TosterDash@users.noreply.github.com>
2020-10-05 21:36:39 -0600

    Prueba com
    aaaa

--- InventarioDasc/JS/editorMap.js
+++ InventarioDasc/JS/editorMap.js
@@ -11,6 +11,8 @@ var xyCoord = [];
 var polyArray = [];
 //Limite de poligonos por creacion
 var numSide = 4;
+//variable de prueba
+var numnumnumn = 1000;
 
 //'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
 //'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
--- InventarioDasc/editMap.php
+++ InventarioDasc/editMap.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <!--dawudnawiunawdi-->
     <!--P5 LIBRARY-->
 	
 	<!-- /<script src="sketch.js"></script> -->
