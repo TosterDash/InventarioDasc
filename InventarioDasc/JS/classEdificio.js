332ae7bd4b214658b2cc7dd90c5be72814d6eb83
JNavAnt <43210295+JNavAnt@users.noreply.github.com>
2020-10-12 15:56:28 -0600

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/TosterDash/InventarioDasc"""
    This reverts commit a03680dbc28352c273ec52c017becbdc4196ab05.

---
+++ InventarioDasc/JS/classEdificio.js
@@ -0,0 +1,53 @@
+var nombre
+class Edificio {
+    constructor(idEdificio,nombre,poly){
+        this.idEdificio = idEdificio;
+        this.nombre = nombre;
+        this.poly = poly;
+
+        
+        
+    }
+    //Metodo para que este poligono muestre sus aulas
+    functionClick(cont,edificiosArray,aulasArray,mapUabcs){
+
+        this.poly.on('click',function(event) {
+            for(let i = 0; i<aulasArray.length;i++){
+                mapUabcs.removeLayer(aulasArray[i].poly);
+            }
+            
+            for(let i = 0; i<aulasArray.length;i++){
+                if(aulasArray[i].edificio == edificiosArray[cont].idEdificio){
+                    console.log("Aulas aqui: "+aulasArray[i].nombre);
+                    aulasArray[i].poly.addTo(mapUabcs);
+                }
+            }
+            
+        })
+
+    }
+    //Metodo para que el poligono se elimine junto con sus aulas añadidas
+    clickToDelete(cont,edificiosArray){
+        this.poly.on('click',function(event) {
+            
+            var option = confirm("Seguro que desea eliminar el edificio "+edificiosArray[cont].nombre  +"? Los edificios eliminaran las aulas que tengan.");
+            if(option){
+                var idEdificio = edificiosArray[cont].idEdificio;
+                var op = "edificio";
+                $.post('PHP/deletePoly.php',{idEdificio,op} ,function(response){
+                    console.log(response);
+                    alert("Se ha eliminado el poligono correctamente");
+                    $(location).attr('href','editMap.php');
+               
+              });
+            }
+        })
+    }
+    //Eliminar el evento click en el poligono
+    deleteClickOnPoly(){
+        this.poly.off();
+    }
+
+    
+    
+}
--- InventarioDasc/mapa/PHP/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-//linea para conectar a la base de datos
-$conexion = mysqli_connect("localhost", "root", "", "inventario");
-
-
-?>
