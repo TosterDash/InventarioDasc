c9b1b4aadcf6889b9f75d99d512f3e1ec618f40e
JNavAnt <43210295+JNavAnt@users.noreply.github.com>
2020-10-28 23:27:26 -0700

    invadd changes

--- InventarioDasc/inventario/PHP/addInventory.php
+++ InventarioDasc/inventario/PHP/addInventory.php
@@ -9,10 +9,15 @@
         $resp = $_POST["resp"];
         $lastMant = $_POST["lastMant"];
         $nextMant = $_POST["nextMant"];
+
+        $insert=("INSERT INTO objeto (Nombre, Descripcion, Cantidad, lastMant, nextMant, mantResp, idTipoCategoria) VALUES ('$name', '$desc', 0,'$lastMant', '$nextMant', '$resp',null)")
     }else if($value == "Consumible"){
         $name = $_POST["name"];
         $desc = $_POST["desc"];
         $cant = $_POST["cant"];
+        $insert=("INSERT INTO objeto (Nombre, Descripcion, Cantidad, lastMant, nextMant, mantResp, idTipoCategoria) VALUES ('$name', '$desc', '$cant', null, null, null, null)")
     }
+    
+    $result = mysqli_query($conexion, $insert);
     
 ?>
