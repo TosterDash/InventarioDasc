23fada6a4cda1d4228d6dfe5389236d06708111d
TosterDash <43210121+TosterDash@users.noreply.github.com>
2020-11-04 15:42:20 -0700

    update addInventory all

--- InventarioDasc/addInventory.php
+++ InventarioDasc/addInventory.php
@@ -1,85 +1,110 @@
 <html>
-    <meta charset="utf-8">
+    
 
     
     
     <title>AÑADIR ARTICULO | Inventario</title>
 <head>
 
-<?php include ('header.html');?>
-    <script
-      src="https://code.jquery.com/jquery-3.5.1.js"
-      integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-      crossorigin="anonymous">
-    </script>
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php include ('header.html');?>
+        <script
+        src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous">
+        </script>
 
-    <link rel="stylesheet" href="../styles/normalize.css">
-    <link rel="stylesheet" href="../styles/inventory.css">
-    <link rel="stylesheet" href="../styles/generalStyle.css">
-    <script src="inventario/JS/addinventory.js"></script>
-</head>
+        <link rel="stylesheet" href="../styles/bootstrap.min.css">
+        <link rel="stylesheet" href="../styles/normalize.css">
+        <link rel="stylesheet" href="../styles/inventory.css">
+        <link rel="stylesheet" href="../styles/generalStyle.css">
+        <link rel="stylesheet" href="alertify/css/alertify.css">
+        <script src="alertify/alertify.js"></script>
+        <script src="inventario/JS/addinventory.js"></script>
+    </head>
 
-<body>
-     <!--Forma para agregar a inventario-->
-    <h1 id="title">INVENTARIO</h1>
-    <div class="basic-form-container">
-        <div class="add-container form-format selector">
-            <select id="tipo_objeto"><!--Combo box para definir si el objeto es equipo o Consumible-->
-                <option value="selecciona">--Selecciona--</option>
-                <option value="Equipo">Equipo</option>
-                <option value="Consumible">Consumible</option>
-            </select>
-        </div>
-        <div class="add-container form-format cons"><!--Si el objeto es de tipo consumible-->
-            <label>Nombre</label><!--Nombre del objeto-->
-            <input type="text" class="input" id="name-form-cons">
-            <label>Descripción</label><!--Descripcion del objeto-->
-            <input type="text" class="input" id="desc-form-cons">
-            <label>Cantidad</label><!--Cantidad del ojeto-->
-            <input type="text" class="input" id="cant-form-cons">
-        </div>
-        <div class="add-container equip"><!--Si el objeto es de tipo equipo-->
-            <div class="form-container form-format">
-                <label>Selecciona categoría</label>
-                <select name="catalogue" id="catalogue"><!--Selecciona que tipo de equipo es-->
-                    <option value="selecciona">--Selecciona--</option>
-                    <option value="impresora">Impresora</option>
-                    <option value="computador">Computador</option>
-                    <option value="cañon">Cañón</option>
-                </select>
-                <label>Nombre</label><!--Nombre del objeto-->
-                <input type="text" class="input" id="name-form-equip">
-                <label>Descripción</label><!--Descripcion del objeto-->
-                <input type="text" class="input" id="desc-form-equip">
+    <body>
+        <form method="POST" id="formSend" enctype="multipart/form-data">
+        <div class="container d-flex justify-content-center">
+                <div class="row pr-5" id="col-1">
+                    <div class="col-sm border">
+                        <div id="col-1-content">
+                            <label class="d-block">Tipo de clasificacion</label>
+                            <select class="d-block form-control " name="col-1-combobox-category" id="col-1-combobox-clasification">
+                                
+                            </select>
+
+                            <label class="d-block">Tipo de producto</label>
+                            <select class="d-block form-control" name="col-1-combobox-product" id="col-1-combobox-product">
+                                
+                            </select>
+
+                            <div id="col-1-block-mant">
+                                <div>
+                                    <input type="checkbox" id="col-1-checkbox-mant">Mantenimiento</input>
+                                </div>
+                                <div>
+                                    <input type="checkbox" id="col-1-checkbox-loan">Disponible para prestamo</input>
+                                </div>
+                                
+
+                            </div>
+
+                            <div class="text-center py-5 ">
+                                <button type="submit" class="btn btn-secondary" id="submit-objeto">Añadir Objeto</button>
+                            </div>
+
+                        </div>
+                        
+                
+                    </div>
+                    <!-- SEGUNDA COLUMNA DE LA PAGINA -->
+                    <div class="col-sm border">
+                        <div id = "col-2-content">
+                            <div id="col-2-block-name">
+                                <label class="d-block">Nombre</label>
+                                <input type="text" name="col-2-text-name" class="form-control d-block" id="col-2-text-name" required></input>
+
+                                <label class="d-block">Descripción</label>
+                                <input type="text" name="col-2-text-desc" class="form-control d-block" id="col-2-text-desc" required></input>
+                            </div>
+
+                            <div id="col-2-block-mant">
+                                <label class="d-block">Responsable del mantenimiento</label>
+                                <input type="text" name="col-2-text-mantResp"  class="form-control d-block" id="col-2-text-mantResp"></input>
+
+                                <label class="d-block">Ultimo mantenimiento</label>
+                                <input type="datetime-local" name="col-2-date-lastMant" class="form-control d-block" id="col-2-date-lastMant"></input>
+
+                                <label class="d-block">Siguiente mantenimiento</label>
+                                <input type="datetime-local" name="col-2-date-nextMant" class="form-control d-block" id="col-2-date-nextMant"></input>
+
+                            </div>
+
+                            <div id="col-2-block-cant">
+                                <label class="d-block">Cantidad del consumible</label>
+                                <input type="number" name="col-2-number-cant" class="form-control d-block" id="col-2-number-cant"></input>
+                            </div>
+
+                        </div>
+                        
+                
+                    </div>
+                    <!-- TERCERA COLUMNA DE LA PAGINA -->
+                    <div class="col-sm border">
+                        <div id="col-3-content">  
+                            <input type="file" class="form-control-file" name="item_file" id="item_file" require></input>
+                        </div>
+                        
+                
+                    </div>
+                
+                </div>
             </div>
-        </div>
-        <div class="add-container form-format equip">
-            <input type="checkbox" id="mantCB" name="mant" value=false>
-            <label for="name">Mantenimiento:</label><br><!--Selecciona si requiere mantenimiento-->
-            <input type="checkbox" id="prestCB" name="prest" value="Prestamo">
-            <label for="name">Disponible para prestamo:</label><br><!--Selecciona si puede ser prestado-->
-        </div>
-        <div class="add-container form-format mant equip" id="mantForm" ><!--Si requiere mantenimiento-->
-            <div class="form-container">
-                <label>Responsable</label><!--Quien es el responsable del mantenimiento-->
-                <input type="text" class="input" id="resp-form">
-                <label>Último mantenimiento</label><!--Cuando fue el ultimo (o primer) mantenimiento-->
-                <input type="datetime-local" class="input" id="lastMant-form" name="lastMant">
-                <label>Siguiente mantenimiento</label><!--Cuando sera el siguiente mantenimiento-->
-                <input type="datetime-local" class="input" id="nextMant-form", name="nextMant">
-            </div>
-        </div>
-        <div>
-            <label for="name">Imagen del articulo:</label><br>
-            <img src="" alt=""><!--Selecciona la imagen del articulo-->
-            <input type="file" name="item_file" id="item_file">
-        </div>
-        <div>
-            <button id="inv-add-cancel">Cancelar</button><!--Boton para cancelar la adicion del articulo-->
-            <button value="Agregar" id="inv-add-btn">Agregar</button><!--Boton para agregar el articulo-->
-        </div>
-    </div>
-</body>
+        </form>     
+    </body>
 
 
 </html>
--- InventarioDasc/inventario/JS/addInventory.js
+++ InventarioDasc/inventario/JS/addInventory.js
@@ -1,41 +1,200 @@
 $(document).ready(function(){
-    $('.cons').hide();
-    $('.equip').hide();
-    $(document).on('change','#tipo_objeto',function(){
-        var value = $('#tipo_objeto').val();
-       if(value ==  "Equipo"){
-            $('.cons').hide();
-            $('.equip').show();
-            $('#mantForm').hide();
-            $('#mantCB').prop('checked', false);
-       }else if(value ==  "Consumible"){
-            $('.cons').show();
-            $('.equip').hide();
-       }
-    })
+    //console.log("dawdaw");
+    //Ocultar elementos de la segunda columna
+    $("#col-2-block-mant").hide();
+    $("#col-2-block-cant").hide();
+    $("#col-1-block-mant").hide();
+    
+    getComboboxCol1();
 
-    $('#mantCB').on('change',function(){
-        mantForm.style.display = this.checked ? 'block' : 'none';
+    $("#col-1-combobox-clasification").on('change',function(){
+       
+        switch($("#col-1-combobox-clasification").val()){
+            case "1":
+                
+                //quitar otros required
+                $("#col-2-number-cant").prop('required',false);
+                //--------------------------------------------
+                $("#col-2-number-cant").val("");
+                $("#col-2-block-cant").hide();
+                $("#col-1-block-mant").show();
+                
+            break;
+
+            case "2":
+                //required a input cantidad
+                $("#col-2-number-cant").prop('required',true);
+                //quitar otros required
+                $("#col-2-text-mantResp").prop('required',false);
+                $("#col-2-date-lastMant").prop('required',false);
+                $("#col-2-date-nextMant").prop('required',false);
+
+                //--------------------------------------------
+                $("#col-2-text-mantResp").val("");    
+                $("#col-2-date-lastMant").val("");    
+                $("#col-2-date-nextMant").val("");
+                $('#col-1-checkbox-mant').prop('checked', false);
+                $("#col-2-block-cant").show();
+                $("#col-1-block-mant").hide();
+                $("#col-2-block-mant").hide();
+            break;
+
+            default:
+                //quitar otros required
+                $("#col-2-text-mantResp").prop('required',false);
+                $("#col-2-date-lastMant").prop('required',false);
+                $("#col-2-date-nextMant").prop('required',false);
+
+                $("#col-2-number-cant").prop('required',false);
+                //--------------------------------------------
+                $("#col-2-number-cant").val("");
+                $("#col-2-text-mantResp").val("");    
+                $("#col-2-date-lastMant").val("");    
+                $("#col-2-date-nextMant").val("");
+                $('#col-1-checkbox-mant').prop('checked', false);
+                $("#col-2-block-mant").hide();
+                $("#col-2-block-cant").hide();
+                $("#col-1-block-mant").hide();
+            break;
+        }
     });
 
-    $(document).on('click', '#inv-add-btn',function(){
-        var value = $('#tipo_objeto').val();
+    $('#col-1-checkbox-mant').on('change',function(){
+        if($('#col-1-checkbox-mant').prop('checked')==true){
+            //required a los inputs de mantenimiento
+            $("#col-2-text-mantResp").prop('required',true);
+            $("#col-2-date-lastMant").prop('required',true);
+            $("#col-2-date-nextMant").prop('required',true);
+            $("#col-2-block-mant").show();
+        }else{
+            //quitar required a los inputs
+            //required a los inputs de mantenimiento
+            $("#col-2-text-mantResp").prop('required',false);
+            $("#col-2-date-lastMant").prop('required',false);
+            $("#col-2-date-nextMant").prop('required',false);
 
-        if(value ==  "Equipo"){
-            var category = $('#catalogue').val();
-            var name = $('#name-form-equip').val();
-            var desc = $('#desc-form-equip').val();
-            var resp = $('#resp-form').val();
-            var lastMant = $('#lastMant-form').val();
-            var nextMant = $('#nextMant-form').val();
-            $.post('inventario/PHP/addinventory.php',{value, category, name, desc, resp, lastMant, nextMant} ,function(response){});
-        }else if(value ==  "Consumible"){
-            var name = $('#name-form-equip').val();
-            var desc = $('#desc-form-equip').val();
-            var cant = $('#cant-form-cons').val();
-            $.post('inventario/PHP/addinventory.php',{value, name, desc, cant} ,function(response){});
+            $("#col-2-text-mantResp").val("");    
+            $("#col-2-date-lastMant").val("");    
+            $("#col-2-date-nextMant").val("");
+            $("#col-2-block-mant").hide();
         }
-    })
+    });
+
+    $("#formSend").on("submit",function(e){
+        e.preventDefault();
+        if($("#col-1-combobox-clasification").val()!=""){
+            
+            if($("#col-1-combobox-product").val()!=""){
+                var form = document.querySelector('form');
+                var formData = new FormData(form);
+                
+                $.ajax({
+                    url: "inventario/PHP/addinventory.php",
+                    type: "POST",
+                    dataType: "HTML",
+                    data: formData,
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                }).done(function(response){
+                    console.log(response);
+                    vaciarCampos();
+                    alertify.success('Se añadio el objeto'); 
+                        
+                        
+                });
+            }else{
+                alertify.warning("No hay un producto seleccionado");
+            }
+        }else{
+            alertify.warning("No hay una clasificacion seleccionada");
+        }
+    });
+
+
+
+
+
+    //funciones---------------------------
+    function getComboboxCol1(){
+        //OPCION categoria PARA CATEGORIA
+        var option = "clasificacion";
+        //ajax para combobox clasificacion
+        $.ajax({
+            url:"inventario/PHP/addinventory.php" ,
+            type:"POST" ,
+            data: {option},
+
+        }).done(function(e){
+            //console.log(e);
+            switch(e){
+
+                case "error":
+                    alertify.error("Error addinventory.PHP-clasificacion");
+                break;
+
+                default:
+                    //console.log("succes");
+                    var category = JSON.parse(e);
+                    var template = `<option value="">--Seleccione--</option>`;
+                    category.forEach(task=>{
+                        template+= `<option value="${task.idClasificacion}">${task.clasificacion}</option>`
+                    })
+
+                    $("#col-1-combobox-clasification").html(template);
+                break;
+            }
+            
+        }).fail(function(e){
+            console.log("FALLO POST DE GET CATEGORY");
+        })
+
+        //ajax para combobox producto
+        option = "producto";
+        $.ajax({
+            url:"inventario/PHP/addinventory.php" ,
+            type:"POST" ,
+            data: {option},
+
+        }).done(function(e){
+            //console.log(e);
+            switch(e){
+
+                case "error":
+                    alertify.error("Error addinventory.PHP-producto");
+                break;
+
+                default:
+                    console.log("succes");
+                    var category = JSON.parse(e);
+                    var template = `<option value="">--Seleccione--</option>`;
+                    category.forEach(task=>{
+                        template+= `<option value="${task.idCategoria}">${task.categoria}</option>`
+                    })
+
+                    $("#col-1-combobox-product").html(template);
+                break;
+            }
+            
+        }).fail(function(e){
+            console.log("FALLO POST DE GET CATEGORY");
+        })
+
+    }
+
+    function vaciarCampos(){
+
+        $("#col-1-combobox-category").val("");
+        $("#col-1-combobox-product").val("");    
+        $("#col-2-text-name").val("");    
+        $("#col-2-text-desc").val("");    
+        $("#col-2-text-mantResp").val("");    
+        $("#col-2-date-lastMant").val("");    
+        $("#col-2-date-nextMant").val("");    
+        $("#col-2-number-cant").val("");    
+        $("#item_file").val("");        
+    }
+
 
 })
 
