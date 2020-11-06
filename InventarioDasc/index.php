f4a1067fae35271f1e96f9d4c675b55fea9437a8
pish18 <paulasoqui69@gmail.com>
2020-11-05 19:33:46 -0700

    JQuery y Popper
    :)

--- InventarioDasc/index.php
+++ InventarioDasc/index.php
@@ -8,9 +8,7 @@ if(!isset($_SESSION['nombre']))
 	header("Location:http://localhost/colection/InventarioDasc/login-registro.php");
 }
 */
-
 ?>
-
 <!DOCTYPE html>
 <html>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -19,14 +17,14 @@ if(!isset($_SESSION['nombre']))
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php include ('header.html');?>
 	<!--SITE STYLE SHEETS-->
-
 	<script
-        src="../Jquery/Jquery.js">
+        src="Jquery/Jquery.js">
     </script>
-    <link rel="stylesheet" href="../styles/bootstrap.min.css">
-	<link rel="stylesheet" href="../styles/bootstrap-4.5.3-dist/css/bootstrap.min.css"></link>
+    <script src="../styles/popper.js"></script>
+    <script src="../styles/bootstrap-4.5.3-dist/css/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="../styles/bootstrap-4.5.3-dist/css/bootstrap.min.css">
   	<script 
-  		src="../styles/bootstrap-4.5.3-dist/js/bootstrap.min.js">s
+  		src="../styles/bootstrap-4.5.3-dist/js/bootstrap.min.js">
   	</script>
 
 	<link rel="stylesheet" href="../styles/normalize.css">
@@ -34,7 +32,6 @@ if(!isset($_SESSION['nombre']))
 	<link rel="stylesheet" href="../styles/hover.css">
 	<title>INICIO</title>
 <head>
-
 </head>
 <body>
 	<div id="demo" class="carousel slide mt-9" data-interval="5000" data-ride="carousel">
