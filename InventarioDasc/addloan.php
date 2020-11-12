018ef3b1f3cd360147c9bc5dc52a26698186ded9
pish18 <paulasoqui69@gmail.com>
2020-11-12 15:49:49 -0700

    Estilo a addloan
    Cambios mayores en el diseño de la página "addloan.php"

--- InventarioDasc/addloan.php
+++ InventarioDasc/addloan.php
@@ -15,6 +15,7 @@
     </script>
     <link rel="stylesheet" href="../styles/normalize.css">
     <link rel="stylesheet" href="../styles/generalStyle.css">
+    <link rel="stylesheet" href="../styles/loanStyle.css">
     <script 
         src="prestamo/JS/addloan.js">
     </script>
@@ -24,34 +25,50 @@
         <h1 class="center-title" >AÑADIR PRÉSTAMO</h1>
         <!--Form para agregar a inventario-->
         <form enctype="multipart/form-data">
-            <div>
-                <label for="loan-add-ident">Identificador:</label><br>
-                <input type="text" id="loan-add-ident" name="loan-add-ident" value=""><br>
-                <!--Edificio-->
-                <select name="loan-add-edif" id="loan-add-edif">
-                    <option value="Macro">Macro</option>
-                    <option value="DASC">Dasc</option>
-                </select>
-                <!--Salon-->
-                <select name="loan-add-clasroom" id="loan-add-clasroom">
-                    <option value="CS1">CS1</option>
-                    <option value="CS2">CS2</option>
-                    <option value="CS3">CS3</option>
-                </select>
-                <!--Fecha del prestamo-->
-                <input id="loan-add-datetime" type="datetime-local">
-            </div>
-        
-                <!--Tabla donde puedes seleccionar todos los objetos que vas a pedir-->
-            <div>
-                <label>Selecciona todos los materiales que necesites</label>
-                <div>cosa lista rara</div>
-                <div>
-                    <!--Boton para cancelar el prestamo-->
-                    <button id="loan-add-cancel"> Cancelar</button>
-                    <!--Boton para agregar el prestamo-->
-                    <button id="loan-add-add"> Agregar</button>
+            <div class="loan-container">
+                <div class="loan-data float-left">
+                    <label class="input-margin" >ID de alumno o maestro</label>
+                    <input class="input-margin" type="number" value="">
+                    <!--Edificio-->
+                    <label class="input-margin" >Edificio</label>
+                    <select class="input-margin" name="loan-add-edif" id="loan-add-edif">
+                        <option value="Macro">Macro</option>
+                        <option value="DASC">Dasc</option>
+                    </select>
+                    <!--Salon-->
+                    <label class="input-margin" >Salón</label>
+                    <select class="input-margin" name="loan-add-clasroom" id="loan-add-clasroom">
+                        <option value="CS1">CS1</option>
+                        <option value="CS2">CS2</option>
+                        <option value="CS3">CS3</option>
+                    </select>
+                    <!--Fecha del prestamo-->
+                    <label class="input-margin" >Fecha salida</label>
+                    <input class="input-margin" id="loan-add-datetime" type="datetime-local">
+                    <label class="input-margin" >Fecha retorno</label>
+                    <input class="input-margin" id="loan-add-datetime" type="datetime-local">
                 </div>
+            
+                <!--Tabla donde puedes seleccionar todos los objetos que vas a pedir-->
+                <div class="float-left">
+                    <label>Selecciona todos los materiales que necesites</label>
+                    <table class="">
+                        <!--fila-->
+                        <tr class="table-row">
+                            <th>
+                                <input type="checkbox" name="articleCheck">
+                                <label>ARTICULO</label>
+                            </th>
+                        </tr>
+
+                    </table>
+                    <div>
+                        <!--Boton para cancelar el prestamo-->
+                        <a href="consloan.php" class="button">Cancelar</a>
+                        <!--Boton para agregar el prestamo-->
+                        <button id="loan-add-add"> Agregar</button>
+                    </div>
+                </div> 
             </div>
         </form> 
     </body>
