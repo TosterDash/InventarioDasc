914e9526422913bb58c8b7b5b4d5d8e175a55ba1
JNavAnt <43210295+JNavAnt@users.noreply.github.com>
2020-11-23 21:03:35 -0700

    addloan ahora registra id de edificio y aula en vez de nombre

--- InventarioDasc/inventario/PHP/adloan.php
+++ InventarioDasc/inventario/PHP/adloan.php
@@ -7,17 +7,17 @@
     $exitDate = $_POST["exitDate"];
     $returnDate = $_POST["returnDate"];
     //Crear query de insert
-    $insert=("INSERT INTO objeto (edificio, aula, exitDate, returnDate) VALUES ('$building', '$classroom', '$exitDate','$returnDate')")
+    $query = mysqli_query($conexion, "SELECT idEdificio from edificio where Nombre  = '$building'");
+    $result = mysqli_fetch_array($query);
+    $idbld = $result['idEdificio'];
+    
+
+    $result = mysqli_query($conexion, "SELECT idAula from aula where nombreAula  = '$classroom'");
+    $result = mysqli_fetch_array($query);
+    $idcls = $result['idAula'];
+
+    $insert=("INSERT INTO objeto (edificio, aula, exitDate, returnDate) VALUES ('$idbld', '$idcls', '$exitDate','$returnDate')")
     $objetos = $_POST["objetos"];
-    //for que recorre el arreglo de objetos que estaran en el prestamo
-   /* for(var i; i<$objetos.length){
-       //consultar la tabla de objetos donde el nombre del objeto sea igual al objeto en la posicion i del arreglo
-        $idobjeto = mysqli_query($conexion, "SELECT idobjeto from objeto where Nombre LIKE '%$objetos[i]%' ");
-        //consulta la tabla prestamo para obtener el registro que se acaba de crear
-        //COMO HAGO ESTO?
-        $idprestamo = mysqli_query($conexion, "SELECT idprestamo from prestamo where Nombre LIKE '%$objetos[i]%' ");
-        //$insert=("INSERT INTO objeto_has_prestamo (idobjeto, idprestamo)
-    }*/
     
     //Insertar a la base de datos
     $result = mysqli_query($conexion, $insert);
--- InventarioDasc/prestamo/PHP/adloanins.php
+++ InventarioDasc/prestamo/PHP/adloanins.php
@@ -7,7 +7,28 @@
     $returnDate = $_POST["returnDate"];
     $idPrestamo = 0;
 
-    $result = mysqli_query($conexion, "INSERT INTO prestamo (building,classroom,exitDate,returnDate) VALUES ('$building','$classroom','$exitDate','$returnDate')");
+    $query = mysqli_query($conexion, "SELECT idEdificio from edificio where Nombre  = '$building'");
+    if($query){
+        $result = mysqli_fetch_array($query);
+        $idbld = $result['idEdificio'];
+        
+    }else{
+        echo "error";
+    }
+    
+    
+
+    $query = mysqli_query($conexion, "SELECT idAula from aula where nombreAula  = '$classroom'");
+    if($query){
+        
+        $result = mysqli_fetch_array($query);
+        $idcls = $result['idAula'];
+        echo $idbld;
+    }else{
+        echo "error";
+    }
+   
+    $result = mysqli_query($conexion, "INSERT INTO prestamo (building,classroom,exitDate,returnDate) VALUES ('$idbld','$idcls','$exitDate','$returnDate')");
     //Insertar a la base de datos
     $result = mysqli_query($conexion, "SELECT idPrestamo FROM prestamo ORDER BY idPrestamo DESC LIMIT 1");
     
