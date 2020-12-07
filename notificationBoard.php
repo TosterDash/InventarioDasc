5c2e66c20e3f979cdf3b84fd2b910a43fa1079cf
pish18 <paulasoqui69@gmail.com>
2020-12-06 20:54:26 -0700

    Update notificationBoard.php

--- notificationBoard.php
+++ notificationBoard.php
@@ -7,7 +7,6 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php include ('header.php');?>
 	<script src="inventario/JS/notificationBoard.js"></script>
 	<link rel="stylesheet" href="styles/normalize.css">
 	<link rel="stylesheet" href="styles/hover.css">
